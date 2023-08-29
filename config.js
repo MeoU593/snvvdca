87feac893583ad5a802d1ebff371bae73023979a
MeoU593 <131269308+MeoU593@users.noreply.github.com>
2023-08-29 14:15:44 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Đào Châu Anh';
+const giftUrl = 'https://www.facebook.com/daochauanh593';
+const eventName = 'sinh nhật vui vẻ';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc cô gái của tôi luôn tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn trong hôm nay và cả những ngày sau. Cảm ơn vì đã ở bên tôi thời gian vừa qua.Sinh nhật tuổi 20 thật vui vẻ nhé';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
