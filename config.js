11a91cc1548dca8174043669c555d8ac0745f432
MeoU593 <131269308+MeoU593@users.noreply.github.com>
2023-08-30 13:08:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const nameGirl = 'Đào Châu Anh';
-const giftUrl = 'https://www.facebook.com/daochauanh593';
+const giftUrl = 'https://www.tiktok.com/@thelastone300823/video/7272990014508764434?lang=en';
 const eventName = 'sinh nhật vui vẻ';
 const titleCard = 'Tặng người ấy';
 const contentCard = 'Chúc cô gái của tôi luôn tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn trong hôm nay và cả những ngày sau. Cảm ơn vì đã ở bên tôi thời gian vừa qua.Sinh nhật tuổi 20 thật vui vẻ nhé';
