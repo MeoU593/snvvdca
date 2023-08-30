b41f4627307c179611ecb77195e16fcc90685ef7
MeoU593 <131269308+MeoU593@users.noreply.github.com>
2023-08-30 13:31:29 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Đào Châu Anh';
 const giftUrl = 'https://www.tiktok.com/@thelastone300823/video/7272990014508764434?lang=en';
 const eventName = 'sinh nhật vui vẻ';
 const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc cô gái của tôi luôn tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn trong hôm nay và cả những ngày sau. Cảm ơn vì đã ở bên tôi thời gian vừa qua.Sinh nhật tuổi 20 thật vui vẻ nhé';
+const contentCard = 'Chúc cô gái của tôi luôn tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Cảm ơn vì đã ở bên anh thời gian vừa qua.Sinh nhật tuổi 20 thật vui vẻ nhé';
 
 
 const giftImage = 'hot-girl.png';
